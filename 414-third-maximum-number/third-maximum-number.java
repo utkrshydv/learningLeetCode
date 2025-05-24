54f55b2cd576e9a49e7257b0232672aff954703c
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2025-05-24 20:58:44 +0530

    Time: 1 ms (97.43%) | Memory: 42.7 MB (94.82%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,9 +1,8 @@
 class Solution {
     public int thirdMax(int[] nums) {
         long first = Long.MIN_VALUE;
-        long second = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE; 
         long third = Long.MIN_VALUE;
-        boolean foundThird = false;  
 
         for (int num : nums) {
             long n = (long) num;  
@@ -15,14 +14,11 @@ class Solution {
                 third = second;
                 second = first;
                 first = n;
-                foundThird = true;
             } else if (n > second) {
                 third = second;
                 second = n;
-                foundThird = true;
             } else if (n > third) {
                 third = n;
-                foundThird = true;
             }
         }
         
