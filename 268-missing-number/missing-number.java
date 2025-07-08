5f76cbfc0e1c1e187ce9a437b6151b2fa2b252aa
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2025-07-08 20:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (43.62%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -2,12 +2,12 @@ class Solution {
     public int missingNumber(int[] nums) {
         int num = nums[0];
 
-        for(int i=1; i<nums.length; i++){
+        for(int i =1; i <nums.length; i++){
             num = num^nums[i];
         }
 
-        for(int j=0; j<=nums.length; j++){
-        num = num^j;
+        for(int j = 0; j <= nums.length; j++){
+            num = num^j;
         }
 
         return num;
