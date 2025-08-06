64b7403444e3b699314f9af9690cb1d79809e5dd
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2025-08-06 20:04:52 +0530

    Time: 84 ms (50.30%) | Memory: 106 MB (34.40%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -1,33 +1,38 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        Map<Integer, Integer> lost = new HashMap<>();
+       Map<Integer, Integer> lossCount = new HashMap<>();
+       Set<Integer> players = new HashSet<>();
 
-        for(int[] it : matches){
-            int lose = it[1];
-            lost.put(lose, lost.getOrDefault(lose, 0) + 1);
+       for(int[] match : matches){
+        int winner = match[0];
+        int loser = match[1];
+
+        players.add(winner);
+        players.add(loser);
+
+        lossCount.put(loser, lossCount.getOrDefault(loser, 0) + 1);
+       }
+
+       List<Integer> zeroLoss = new ArrayList<>();
+       List<Integer> oneLoss = new ArrayList<>();
+
+       for(int player: players){
+        int losses = lossCount.getOrDefault(player, 0);
+         if(losses == 0){
+            zeroLoss.add(player);
+         } else if (losses == 1){
+            oneLoss.add(player);
+         }
         }
-
-        List<Integer> notLost = new ArrayList<>();
-        List<Integer> oneLoss = new ArrayList<>();
-
-        for(int[] it : matches){
-            int lose = it[1];
-            int win = it[0];
-
-            if(lost.get(lose) == 1){
-                oneLoss.add(lose);
-            }
-
-            if(!lost.containsKey(win)){
-                notLost.add(win);
-                lost.put(win, 2);
-            }
-        }
-
-        Collections.sort(notLost);
+        Collections.sort(zeroLoss);
         Collections.sort(oneLoss);
 
-        return Arrays.asList(notLost, oneLoss);
+        List<List<Integer>> answer = new ArrayList<>();
         
+        answer.add(zeroLoss);
+        answer.add(oneLoss);
+
+        return answer;
+       
     }
 }
