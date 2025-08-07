5a8c4d9e80dd62afd7a0517d4413dae26bbe2ce3
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2025-08-07 22:32:03 +0530

    Time: 1 ms (88.34%) | Memory: 41.2 MB (36.49%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.java
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -3,19 +3,13 @@ class Solution {
 
         Map<Integer, Integer> map = new HashMap<>();
 
+        int count = 0;
+
         for(int num : nums){
+            count += map.getOrDefault(num, 0);
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        int count = 0;
-
-        for(int val : map.values()){
-            if(val > 1){
-                count += (val*(val-1))/2;
-            }
-        }
-       
-        
         return count;
     }
 }
