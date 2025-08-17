cc6710933dd3bd3d92cea5e051bacdf8c7acd40c
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2025-08-17 23:05:19 +0530

    Time: 5 ms (65.88%) | Memory: 44.1 MB (46.87%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+
+        int reverse = 0;
+        int copy = x;
+
+        while(x>0){
+            reverse = (reverse*10)+x%10;
+            x/=10;
+        }
+
+        return reverse == copy;
+        
+    }
+}
