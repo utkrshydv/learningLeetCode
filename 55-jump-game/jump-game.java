4ea8c94c3920ab4122b7229f4dc73bd1b26c7ba6
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2025-08-26 22:27:30 +0530

    Time: 1 ms (99.84%) | Memory: 45.9 MB (43.36%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goalPost = nums.length - 1;
+
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(i + nums[i] >= goalPost){
+                goalPost = i;
+            }
+        }
+        return goalPost == 0;
+    }
+}
