42e4f1efdd54996fd4310b339287c73960f59f04
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2025-12-03 19:52:42 +0530

    Time: 15 ms (40.86%) | Memory: 18 MB (48.12%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,21 +1,12 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
-        countS = {}
-        countT = {}
 
-        for char in s:
-            countS[char] = countS.get(char, 0) + 1
+        countS, countT = {}, {}
 
-        for char in t:
-            countT[char] = countT.get(char, 0) + 1
-
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
         return countS == countT
         
