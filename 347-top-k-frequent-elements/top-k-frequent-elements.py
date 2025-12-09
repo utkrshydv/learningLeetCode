d6b95bd47fb0e9ee41409a09e68ece2fc4ce1b6d
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2025-12-09 19:58:30 +0530

    Time: 8 ms (32.33%) | Memory: 24.1 MB (24.45%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,15 +1,33 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
+        count = {}
+
+        freq = [[] for i in range(len(nums)+1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
         res = []
-        hashmap = {}
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
 
-        for num in nums:
-            hashmap[num] = 1 + hashmap.get(num, 0)
-
-        sorted_map = [k for k,v in sorted(hashmap.items(), key = lambda item : item[1], reverse  = True)]
 
 
-        return sorted_map[:k]
+        # res = []
+        # hashmap = {}
+
+        # for num in nums:
+        #     hashmap[num] = 1 + hashmap.get(num, 0)
+
+        # sorted_map = [k for k,v in sorted(hashmap.items(), key = lambda item : item[1], reverse  = True)]
+
+
+        # return sorted_map[:k]
 
                 
