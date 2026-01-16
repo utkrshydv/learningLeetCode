076fcf003b2a8abcbd3621282edb037b16b02509
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2026-01-16 20:23:31 +0530

    Time: 23 ms (5.95%) | Memory: 19.8 MB (24.67%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,8 +1,37 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new_string = ""
-        for char in s:
-            if char.isalnum():
-                new_string += char.lower()
-        return new_string == new_string[::-1]
+        l,r = 0, len(s) - 1
+
+        while l < r :
+            while l < r and not self.isalphanum(s[l]):
+                l += 1
+            while r > l and not self.isalphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l+1, r-1
+        return True
+
+    def isalphanum(self, char):
+            return (ord('A') <= ord(char) <= ord('Z') or
+                    ord('a') <= ord(char) <= ord('z') or
+                    ord('0') <= ord(char) <= ord('9'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # new_str = ""
+        # for ch in s:
+        #     if ch.isalnum():
+        #         new_str += ch.lower()
+        # return new_str == new_str[::-1]
         
