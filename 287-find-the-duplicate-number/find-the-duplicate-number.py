2b291fccf51ea754b235c69b8ee780a9fb3038ef
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2026-01-28 20:45:25 +0530

    Time: 23 ms (79.74%) | Memory: 33.6 MB (37.69%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,11 +1,41 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        freq = defaultdict(int)
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
 
-        for num in nums:
-            freq[num] += 1
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
 
-        for k, v in freq.items():
-            if v > 1:
-                return k
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # should not have been accepted. 
+        
+        # freq = defaultdict(int)
+
+        # for num in nums:
+        #     freq[num] += 1
+
+        # for k, v in freq.items():
+        #     if v > 1:
+        #         return k
         
