a07cdc9978453b9aae01d5eb71bb754c49f4b75d
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2026-05-07 21:10:41 +0530

    Time: 3 ms (7.48%) | Memory: 20.6 MB (7.66%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -14,16 +14,30 @@ class Solution:
         #     curr = nxt
         # return prev
 
+        # if not head:
+        #     return None
+            
+        # newHead = head
+        # if head.next:
+        #     newHead = self.reverseList(head.next)
+        #     head.next.next = head
+        # head.next = None
+
+        # return newHead
+
         if not head:
             return None
-            
-        newHead = head
-        if head.next:
-            newHead = self.reverseList(head.next)
-            head.next.next = head
-        head.next = None
 
-        return newHead
+        prev, curr = None, head
+
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        return prev
 
 
 
