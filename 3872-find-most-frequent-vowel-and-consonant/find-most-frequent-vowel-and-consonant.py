a017f425927c798f46f4ef753853962ee28f9082
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2026-05-12 21:04:23 +0530

    Time: 1 ms (68.08%) | Memory: 19.1 MB (90.94%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -1,16 +1,33 @@
 class Solution:
     def maxFreqSum(self, s: str) -> int:
-        v_count = {}
-        c_count = {}
-        vowels = ['a', 'e', 'i', 'o', 'u']
+        # v_count = {}
+        # c_count = {}
+        # vowels = ['a', 'e', 'i', 'o', 'u']
+
+        # for char in s:
+        #     if char in vowels:
+        #         v_count[char] = v_count.get(char, 0) + 1
+        #     else:
+        #         c_count[char] = c_count.get(char, 0) + 1
+
+        # count = max(v_count.values(), default=0) + max(c_count.values(), default=0)
+
+        # return count
+
+        freq = {}
+        vowels = {'a', 'e', 'i', 'o', 'u'}
 
         for char in s:
+            freq[char] = freq.get(char, 0) + 1
+
+        max_v = 0
+        max_c = 0
+
+        for char in freq:
             if char in vowels:
-                v_count[char] = v_count.get(char, 0) + 1
+                max_v = max(max_v, freq[char])
             else:
-                c_count[char] = c_count.get(char, 0) + 1
+                max_c = max(max_c, freq[char])
 
-        count = max(v_count.values(), default=0) + max(c_count.values(), default=0)
-
-        return count
+        return max_v + max_c
         
