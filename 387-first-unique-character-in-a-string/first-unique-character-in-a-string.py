d24ca71fc5250cebcb86a69060c04401df8ad221
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2026-08-18 16:13:35 +0530

    Time: 27 ms (98.81%) | Memory: 19.8 MB (22.71%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,14 +1,14 @@
+from collections import Counter
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+
+        freq = Counter(s)
+        sorted_freq = sorted(freq.items(), key = lambda x : x[1])
         char_arr = [0]*26
 
-        for c in s:
-            char_arr[ord(c) - ord("a")] += 1
-        
-        for i, c in enumerate(s):
-            if char_arr[ord(c) - ord("a")] == 1:
-                return i
-            
+        if sorted_freq[0][1] == 1:
+            return s.index(sorted_freq[0][0])
         
         return -1
         
