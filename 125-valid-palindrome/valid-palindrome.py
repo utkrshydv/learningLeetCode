c62f8dcb0b1c6ad36b716f020d2d3310efcfbc00
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2026-08-18 17:50:16 +0530

    Time: 3 ms (99.04%) | Memory: 20.2 MB (25.86%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,38 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
 
-        while l < r:
-            while l < r and not self.isalphanum(s[l]):
-                l += 1
-            while r > l and not self.isalphanum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l, r = l + 1, r - 1
-        return True
+        res = "".join([char for char in s if char.isalnum()])
+        res=res.lower()
+        return res == res[::-1]
+    #     l, r = 0, len(s) - 1
 
+    #     while l < r:
+    #         while l<r and not self.isalphanum(s[l]):
+    #             l += 1
+    #         while r > l and not self.isalphanum(s[r]):
+    #             r -= 1
+    #         if s[l].lower() != s[r].lower():
+    #             return False
+    #         l, r = l+1, r-1
+    #     return True
 
-    def isalphanum(self, char):
-        return (ord('A') <= ord(char) <= ord('Z') or
-                ord('a') <= ord(char) <= ord('z') or
-                ord('0') <= ord(char) <= ord('9'))
-
-
-
-
-
+    # def isalphanum(self, char: str) -> bool:
+    #     return (ord('A') <= ord(char) <= ord('Z') or
+    #             ord('a') <= ord(char) <= ord('z') or
+    #             ord('0') <= ord(char) <= ord('9'))
 
 
 
-
-
-
-
-
-        # new_str = ""
-        # for ch in s:
-        #     if ch.isalnum():
-        #         new_str += ch.lower()
-        # return new_str == new_str[::-1]
-        
+   
