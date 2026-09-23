1e660b1bf8484c1e0acbd9f4f292036940e25b31
utkrshydv <139888051+utkrshydv@users.noreply.github.com>
2026-09-23 15:04:51 +0530

    Time: 3 ms (19.06%) | Memory: 20.5 MB (6.98%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,7 +1,5 @@
 class Solution:
     def floodFill(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
-       
-
         og = image[sr][sc]
 
         if og == color:
@@ -10,16 +8,14 @@ class Solution:
         def dfs(row, col):
             image[row][col] = color
 
-            dir = [(-1, 0),(1,0), (0,-1), (0, 1)]
+            dir = [(-1, 0), (1,0), (0,-1), (0,1)]
 
             for r, c in dir:
-                new_row = row + r
-                new_col = col + c
+                new_r, new_c = row + r, col + c
 
-                if (0<= new_row < len(image) and 0 <= new_col < len(image[0]) and image[new_row][new_col] == og):
-                    dfs(new_row, new_col)
+                if 0 <= new_r < len(image) and 0 <= new_c < len(image[0]) and image[new_r][new_c] == og:
+                    dfs(new_r, new_c)
 
         dfs(sr, sc)
 
         return image
-        
